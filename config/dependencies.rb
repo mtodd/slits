bf2e1509633ea20aa775c0d86a46285fe881f8aa
Matt Todd <chiology@gmail.com>
2008-10-13 15:32:13 -0400

    Added the slices dependency.

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -3,7 +3,7 @@ dependency "merb-assets", "0.9.9"        # Provides link_to, asset_path, auto_li
 # dependency "merb-cache", "0.9.9"         # Provides your application with caching functions 
 dependency "merb-helpers", "0.9.9"       # Provides the form, date/time, and other helpers
 # dependency "merb-mailer", "0.9.9"        # Integrates mail support via Merb Mailer
-# dependency "merb-slices", "0.9.9"        # Provides a mechanism for letting plugins provide controllers, views, etc. to your app
+dependency "merb-slices", "0.9.9"        # Provides a mechanism for letting plugins provide controllers, views, etc. to your app
 # dependency "merb-auth", "0.9.9"          # An authentication slice (Merb's equivalent to Rails' restful authentication)
  
 dependency "dm-core", "0.9.6"         # The datamapper ORM
