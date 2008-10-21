f130d858db18385e11643e39e367980424c6a0a1
Matt Todd <chiology@gmail.com>
2008-10-21 01:03:46 -0400

    Added the functionality for querying GitHub for Merb-Slice repositories and adding users and repositories.

--- app/models/slice.rb
+++ app/models/slice.rb
@@ -1,9 +1,14 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 class Slice
   include DataMapper::Resource
   
   ### API Call Reference
   
-  GITHUB_QUERY = "http://github.com/api/v1/json/search/merb-slice"
+  GITHUB_SEARCH = "http://github.com/api/v1/json/search/%s"
+  GITHUB_REPOSITORY = "http://github.com/api/v1/json/%s/%s/commits/master"
   
   ### Properties
   
@@ -32,8 +37,57 @@ class Slice
   class << self
     
     def search
-      # HTTP request with GITHUB_QUERY, parsing the JSON
-      # updating or creating (or disabling) slices as appropriate
+      ### GET the repositories from GitHub
+      search_address = GITHUB_SEARCH % ["merb-slice"]
+      uri = URI.parse(search_address)
+      request = Net::HTTP::Get.new(uri.path)
+      repositories = Net::HTTP.start(uri.host, uri.port) do |http|
+        Merb.logger.debug "Searching GitHub: #{search_address}"
+        Mash.new(JSON.parse(http.request(request).body))[:repositories]
+      end
+      Merb.logger.debug "Found #{repositories.size} repositories"
+      
+      ### Parse response
+      repositories.each do |repository|
+        Merb.logger.debug "Processing repository #{repository[:name]}"
+        Merb.logger.debug "#{repository.inspect}"
+        
+        ### establish a user if none exists
+        unless user = User.first(:username => repository[:owner])
+          user = User.new(:username => repository[:owner])
+          user.save
+        end
+        
+        ### retrieve latest commit for repo
+        commits_address = GITHUB_REPOSITORY % [repository[:owner], repository[:name]]
+        uri = URI.parse(commits_address)
+        request = Net::HTTP::Get.new(uri.path)
+        commit = Net::HTTP.start(uri.host, uri.port) do |http|
+          Merb.logger.debug "Pulling commits for #{repository[:name]}: #{commits_address}"
+          Mash.new(JSON.parse(http.request(request).body))[:commits]
+        end.first || {:authored_date => nil} # default to no commit date if no commit has occured
+        Merb.logger.debug "#{commit.inspect}"
+        repository[:last_commit_at] = DateTime.parse(commit[:authored_date]) unless commit[:authored_date].nil?
+        
+        ### compile attributes
+        attributes = {
+          :name => repository[:name],
+          :source => repository[:url],
+          :description => repository[:description],
+          :last_commit_at => repository[:last_commit_at],
+          :user => user
+        }
+        
+        ### determine if slice has been recorded yet
+        unless slice = Slice.first(:source => repository[:url])
+          ### create the new slice
+          slice = Slice.new(attributes)
+          slice.save
+        else
+          ### update details
+          slice.update_attributes(attributes)
+        end
+      end
     end
     
   end
--- config/dependencies.rb
+++ config/dependencies.rb
@@ -6,9 +6,9 @@ dependency "merb-helpers", "0.9.9"       # Provides the form, date/time, and oth
 dependency "merb-slices", "0.9.9"        # Provides a mechanism for letting plugins provide controllers, views, etc. to your app
 # dependency "merb-auth", "0.9.9"          # An authentication slice (Merb's equivalent to Rails' restful authentication)
  
-dependency "dm-core", "0.9.6"         # The datamapper ORM
-# dependency "dm-aggregates", "0.9.6"   # Provides your DM models with count, sum, avg, min, max, etc.
-dependency "dm-migrations", "0.9.6"   # Make incremental changes to your database.
-dependency "dm-timestamps", "0.9.6"   # Automatically populate created_at, created_on, etc. when those properties are present.
-# dependency "dm-types", "0.9.6"        # Provides additional types, including csv, json, yaml.
-dependency "dm-validations", "0.9.6"  # Validation framework
+dependency "dm-core", "0.9.7"         # The datamapper ORM
+# dependency "dm-aggregates", "0.9.7"   # Provides your DM models with count, sum, avg, min, max, etc.
+dependency "dm-migrations", "0.9.7"   # Make incremental changes to your database.
+dependency "dm-timestamps", "0.9.7"   # Automatically populate created_at, created_on, etc. when those properties are present.
+# dependency "dm-types", "0.9.7"        # Provides additional types, including csv, json, yaml.
+dependency "dm-validations", "0.9.7"  # Validation framework
